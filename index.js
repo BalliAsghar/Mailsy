0811a67d04329cb6a2a3f68a9a363afb37e110e3
BalliAsghar <ballihacker@outlook.com>
2022-03-09 16:32:52 +0000

    getting emails

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
-const { createAccount } = require("./utils");
+const { createAccount, fetchMessages } = require("./utils");
 const program = new Command();
 
 // Generate a new email
 program.command("generate").action(() => createAccount());
+
+// fetch messages from the inbox
+program.command("messages").action(() => fetchMessages());
 
 program.parse();
--- utils/index.js
+++ utils/index.js
@@ -8,6 +8,7 @@ exports.createAccount = async () => {
 
   // get the first domain
   const domain = data["hydra:member"][0].domain;
+
   console.log(domain);
   // generate a random email address
   const email = `${Math.random().toString(36).substring(7)}@${domain}`;
@@ -21,14 +22,52 @@ exports.createAccount = async () => {
       password,
     });
 
-    // write the data to file using the fs module
-    await fs.writeFile("./account.json", JSON.stringify(data, null, 2));
+    // add password to the data object
+    data.password = password;
 
     // copy the email to the clipboard
     await copy(email);
+
+    // get Jwt token
+    const { data: token } = await axios.post("https://api.mail.tm/token", {
+      address: email,
+      password,
+    });
+
+    // write token to a data object
+    data.token = token;
+
+    // write the data to file using the fs module
+    await fs.writeFile("./account.json", JSON.stringify(data, null, 2));
 
     console.log(`Account created: ${email}`);
   } catch (error) {
     console.error(error.message);
   }
+};
+
+exports.fetchMessages = async () => {
+  // read the account data from file
+  const account = JSON.parse(await fs.readFile("./account.json"));
+
+  // get the messages
+  const { data } = await axios.get("https://api.mail.tm/messages", {
+    headers: {
+      Authorization: `Bearer ${account.token.token}`,
+    },
+  });
+  // get the emails
+  const emails = data["hydra:member"];
+
+  // if there are no emails, exit
+  if (!emails.length) {
+    console.log("No emails");
+    return;
+  }
+
+  // display the from and subject of the emails
+  emails.forEach((email) => {
+    console.log(`From: ${email.from.name} (${email.from.address})`);
+    console.log(`Subject: ${email.subject}`);
+  });
 };
