ff00dc34e30fe96cacfd71d13f80c7388204a9ab
Balli Asghar <ballihacker@outlook.com>
2022-09-17 23:09:49 +0100

    added Tests

---
+++ __tests__/main.test.js
@@ -0,0 +1,54 @@
+import {describe, expect, it, beforeEach, beforeAll} from "vitest"
+import utils from "../utils/index"
+import fs from "node:fs/promises"
+import { afterAll } from "vitest";
+
+beforeAll(async () => await utils.deleteAccount());
+afterAll(async () => await utils.deleteAccount());
+
+describe("Commands", () => {
+    describe("g", () => {
+        it("should generate a new email", async () => {
+            await utils.createAccount();
+            const data = await fs.readFile("./data/account.json", "utf-8");
+            const account = JSON.parse(data);
+            expect(data).not.toBe(null);
+            expect(account.email).not.toBe(null);
+            expect(account.password).not.toBe(null);
+            expect(account.token.token).not.toBe(null);
+        })
+    });
+    describe("me", () => {
+        it("should show details of the account", async () => {
+            await utils.createAccount();
+            await utils.showDetails();
+            const data = await fs.readFile("./data/account.json", "utf-8");
+            const account = JSON.parse(data);
+            expect(data).not.toBe(null);
+            expect(account.email).not.toBe(null);
+            expect(account.password).not.toBe(null);
+            expect(account.token.token).not.toBe(null);
+
+        })
+    });
+    describe("d", () => {
+        it("should delete the account", async () => {
+            await utils.createAccount();
+            await utils.deleteAccount();
+            await expect(fs.readFile("./data/account.json", "utf-8")).rejects.toThrow("no such file or directory");
+        })
+    });
+    describe("me", () => {
+        it("should show details of the account", async () => {
+            await utils.createAccount();
+            await utils.showDetails();
+            const data = await fs.readFile("./data/account.json", "utf-8");
+            const account = JSON.parse(data);
+            expect(data).not.toBe(null);
+            expect(account.email).not.toBe(null);
+            expect(account.password).not.toBe(null);
+            expect(account.token.token).not.toBe(null);
+
+        })
+    });
+});
